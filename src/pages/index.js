7c6dfc316740794a3be888b76768a39db9c9e56b
egluna-dev <egluna@live.ca>
2022-12-18 00:53:30 +1100

    Added ServiceAndSupport section component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import Layout from "components/Layout";
 import HeroSection from "components/Hero";
 import AboutSection from "components/About";
+import ServicesAndSupport from "components/header/services/ServicesAndSupport";
 import Header from "components/header/Header";
 import Footer from "components/Footer";
 
@@ -10,6 +11,7 @@ export default function Home() {
       <Header />
       <HeroSection />
       <AboutSection />
+      <ServicesAndSupport />
       <Footer />
     </Layout>
   )
