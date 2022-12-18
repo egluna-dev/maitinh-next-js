d484242d2cc85c2cb5bd4cb5fbefd23ce038e95f
egluna-dev <egluna@live.ca>
2022-12-18 15:36:05 +1100

    Removed header from _app.js and added meta tag to Layout Head

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -24,6 +24,7 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,9 +6,6 @@ import Head from 'next/head';
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> 
-      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   )
