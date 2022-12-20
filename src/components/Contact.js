4c5ff5a6c8763698d43bee5c2e83608a1087d411
egluna-dev <egluna@live.ca>
2022-12-20 11:57:49 +1100

    Created contact section

---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+/** @jsxImportSource theme-ui */
+import { Container } from "theme-ui";
+
+export default function ContactSection() {
+    const styles = {
+        
+    }
+    return (
+        <Container sx={styles.container}>
+            <h1>Contact Section</h1>
+        </Container>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import HeroSection from "components/Hero";
 import AboutSection from "components/About";
 import ServicesAndSupport from "components/services/ServicesAndSupport";
 import PricingSection from "components/Pricing";
+import ContactSection from "components/Contact";
 import Header from "components/header/Header";
 import Footer from "components/Footer";
 
@@ -14,6 +15,7 @@ export default function Home() {
       <AboutSection />
       <ServicesAndSupport />
       <PricingSection />
+      <ContactSection />
       <Footer />
     </Layout>
   )
